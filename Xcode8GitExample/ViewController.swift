6e7a2c1a8f59c6c71d96b8a22e7d657b686ae9aa
Aaron Cochran <aaronstuver@aarons-mbp.attlocal.net>
2017-05-20 17:39:27 -0400

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Aaron Cochran on 5/20/17.
+//  Copyright © 2017 Aaron Cochran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
